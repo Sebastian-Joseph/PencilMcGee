dbf423793c03a0437bb8318049b05ae791be0f52
Sebastian Joseph <theclimax25@gmail.com>
2021-12-06 23:47:23 -0500

    added some features. video #2 minute 16:50

---
+++ PencilMcGee/src/main/GamePanel.java
@@ -0,0 +1,88 @@
+package main;
+
+import PencilMcGee.src.main.KeyHandler;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+import java.awt.Graphics2D;
+
+
+public class GamePanel extends JPanel implements Runnable {
+    // Screen Settings
+    final int originalTileSize = 8;
+    final int scale = 4;
+
+    final int tileSize = originalTileSize * scale;
+    final int maxScreenCol = 48;
+    final int maxScreenRow = 27;
+    final int screenWidth = tileSize * maxScreenCol;
+    final int screenHeight = tileSize * maxScreenRow;
+
+    KeyHandler keyH = new KeyHandler();
+    Thread gameThread;
+
+
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+    
+    @Override
+    public void run() {
+
+
+        while(gameThread != null) {
+
+            System.out.println("pee");
+
+            update();
+
+            repaint();
+        }
+    }
+    public void update(){
+        if (keyH.upPressed == true) {
+            playerY -= playerSpeed;
+
+        }
+        else if(keyH.downPressed == true) {
+            playerY += playerSpeed;
+        }
+        else if(keyH.leftPressed == true) {
+            playerX -= playerSpeed;
+        }
+        else if(keyH.rightPressed == true) {
+            playerX += playerSpeed;
+        }
+    }
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        g2.setColor(Color.white);
+
+        g2.fillRect(playerX, playerY, tileSize, tileSize);
+
+        g2.dispose();
+
+    }
+}
+
+
