77f5bdcfd861599b528fecc5f018e3904b0179bd
Sebastian-Joseph <theclimax25@gmail.com>
2021-12-17 14:48:02 -0500

    progressed thriyugh eventHandler

--- PencilMcGee/main/EventHandler.java
+++ PencilMcGee/main/EventHandler.java
@@ -1,9 +1,20 @@
 package main;
 
 import java.awt.Rectangle;
+import java.io.IOException;
 
 public class EventHandler {
+    GamePanel gp;
 
+    {
+        try {
+            gp = new GamePanel();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    Player player = new Player(gp.tileSize / 2, gp.tileSize * 2);
     Rectangle eventRect;
     int eventRectDefaultX,  eventRectDefaultY;
 
@@ -23,7 +34,10 @@ public class EventHandler {
     public boolean hit(int eventCol, int eventRow, String reqDirection) {
 
         boolean hit = false;
-
+        double playerX = player.getXPos();
+        double playerY = player.getYPos();
+        eventRect.x = eventCol*gp.tileSize + eventRect.x;
+        eventRect.y = eventCol*gp.tileSize + eventRect.y;
 
 
         return hit;
